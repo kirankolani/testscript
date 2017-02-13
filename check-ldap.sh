93d99c26b4b47f7bb9fdec8fa823f864402c437c
kirankolani <kirankolani@gmail.com>
2017-02-13 19:29:54 +0530

    Update check-ldap.sh

--- check-ldap.sh
+++ check-ldap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-ldapsearch -h app170.dev.amo.or1.adobe.net -p 389 -D "cn=directory manager"  -s sub -b cn=config -w qazplm123 "(objectclass=nsds5ReplicationAgreement)"
+ldapsearch -h hostname -p 389 -D "cn=directory manager"  -s sub -b cn=config -w qazplm123 "(objectclass=nsds5ReplicationAgreement)"
 
